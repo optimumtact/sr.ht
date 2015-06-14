63b00cdca9e1b4571336019dd19bb90ab6ba0ac3
Drew DeVault <sir@cmpwn.com>
2015-06-14 11:10:48 -0600

    Add User.uploads view

--- srht/objects.py
+++ srht/objects.py
@@ -32,6 +32,7 @@ class User(Base):
     comments = Column(Unicode(512))
     approved = Column(Boolean())
     rejected = Column(Boolean())
+    uploads = relationship('Upload', order_by='Mod.id')
 
     def set_password(self, password):
         self.password = bcrypt.hashpw(password, bcrypt.gensalt())
