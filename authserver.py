5fca9cd922308a85c9c895fa9c233731a1a1df82
Drew DeVault <sir@cmpwn.com>
2015-06-25 14:21:20 -0600

    Flesh out auth server a bit more

--- authserver.py
+++ authserver.py
@@ -4,12 +4,16 @@ import bcrypt
 import asyncore
 from secure_smtpd import SMTPServer, FakeCredentialValidator
 from srht.objects import User
+from sqlalchemy import or_
 
 class UserValidator(object):
     def validate(self, username, password):
-        user = User.query.filter(User.username == username).first()
+        user = User.query.filter(or_(User.username == username, User.email == username)).first()
         if not user:
             print("Authentication failed for {}, unknown user".format(user))
+            return False
+        if not user.approved:
+            print("Authentication failed for {}, account unapproved".format(user))
             return False
         success = bcrypt.hashpw(password, user.password) == user.password
         if not success:
@@ -17,7 +21,6 @@ class UserValidator(object):
         else:
             print("Authentication successful for {}".format(user))
         return success
-
 
 SMTPServer(
     ('0.0.0.0', 4650),
