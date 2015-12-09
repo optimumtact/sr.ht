7b446696c383c4d8ecf2498fd070dab3fbbda276
oranges <email@oranges.net.nz>
2015-12-10 00:13:56 +1300

    Revert "Move the cli script to a better path"
    This reverts commit b1e37e2111384e663a463a86b7ca493c48297ec3.

---
+++ cli_admin.py
@@ -0,0 +1,58 @@
+from srht.database import db
+from srht.objects import User
+from datetime import datetime
+from docopt import docopt
+#Functions driving behaviour
+
+def remove_admin(arguments):
+    u = User.query.filter(User.username == arguments['<name>']).first()
+    if(u):
+        u.admin = False # remove admin
+        db.commit()
+    else:
+        print('Not a valid user')
+
+def make_admin(arguments):
+    u = User.query.filter(User.username == arguments['<name>']).first()
+    if(u):
+        u.admin = True # make admin
+        db.commit()
+    else:
+        print('Not a valid user')
+
+def list_admin(arguments):
+    users = User.query.filter(User.admin == True)
+    for u in users:
+        print(u.username)
+
+def approve_user(arguments):
+    u = User.query.filter(User.username == arguments['<name>']).first()
+    if(u):
+        u.approved = True # approve user
+        u.approvalDate = datetime.now()
+        db.commit()
+    else:
+        print('Not a valid user')
+
+
+interface = """
+Command line admin interface
+Usage:
+    cli_admin admin promote <name>
+    cli_admin admin demote <name>
+    cli_admin admin list
+    cli_admin user approve <name>
+
+Options:
+    -h --help Show this screen.
+"""
+if __name__ == '__main__':
+    arguments = docopt(interface, version='make admin 0.1')
+    if(arguments['admin'] and arguments['promote']):
+        make_admin(arguments)
+    elif(arguments['admin'] and arguments['demote']):
+        remove_admin(arguments)
+    elif(arguments['admin'] and arguments['list']):
+        list_admin(arguments)
+    elif(arguments['user'] and arguments['approve']):
+        remove_admin(arguments)
