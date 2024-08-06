fd4a4b16b69b5c5aeae6a463a55ec576573eecd2
oranges <email@oranges.net.nz>
2024-08-06 16:14:57 +1200

    Update dependencies, start fixing some lint issues
    Ignore the data directory used in production for storage

--- srht/blueprints/html.py
+++ srht/blueprints/html.py
@@ -1,25 +1,30 @@
-from flask import Blueprint, render_template, abort, request, redirect, session, url_for, send_file, Response
+from flask import (
+    Blueprint,
+    render_template,
+    abort,
+    request,
+    redirect,
+    Response,
+)
 from flask_login import current_user, login_user, logout_user
-from sqlalchemy import desc, or_, and_
-from srht.objects import *
-from srht.common import *
+from sqlalchemy import desc
+from srht.objects import User, Upload
 from srht.config import _cfg, _cfgi
+from srht.common import loginrequired, with_session, adminrequired
 from srht.email import send_reset, send_request_notification
+from srht.database import db
 
 from datetime import datetime, timedelta
 import binascii
 import os
-import zipfile
 import urllib
 import re
-import json
 import locale
-import shlex
-import math
-import hashlib
+import bcrypt
 
 encoding = locale.getdefaultlocale()[1]
-html = Blueprint('html', __name__, template_folder='../../templates')
+html = Blueprint("html", __name__, template_folder="../../templates")
+
 
 @html.route("/")
 def index():
@@ -30,49 +35,63 @@ def index():
         free_space = st.f_bavail * st.f_frsize
         total_space = st.f_blocks * st.f_frsize
         used_space = (st.f_blocks - st.f_bfree) * st.f_frsize
-        approvals = User.query.filter(User.approved == False).filter(User.rejected == False).count()
-        return render_template("index-member.html", new=new, total=total, \
-                used_space=used_space, free_space=free_space, total_space=total_space, approvals=approvals)
-    registration=False
+        approvals = User.query.filter(User.approved is False).filter(User.rejected is False).count()
+        return render_template(
+            "index-member.html",
+            new=new,
+            total=total,
+            used_space=used_space,
+            free_space=free_space,
+            total_space=total_space,
+            approvals=approvals,
+        )
+    registration = False
     if _cfg("registration") and _cfg("registration") == "True":
-        registration=True
+        registration = True
     return render_template("index.html", registration=registration)
 
-@html.route("/register", methods=['POST'])
+
+@html.route("/register", methods=["POST"])
 def register():
     errors = list()
-    registration=True
+    registration = True
     if _cfg("registration") and _cfg("registration") != "True":
-        registration=False
-        errors.append('Registration is currently disabled')
+        registration = False
+        errors.append("Registration is currently disabled")
 
-    email = request.form.get('email')
-    username = request.form.get('username')
-    password = request.form.get('password')
-    comments = request.form.get('comments')
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    comments = request.form.get("comments")
     if not email:
-        errors.append('Email is required.')
+        errors.append("Email is required.")
     else:
         if not re.match(r"^[^@]+@[^@]+\.[^@]+$", email):
-            errors.append('Please use a valid email address.')
+            errors.append("Please use a valid email address.")
         if User.query.filter(User.username.ilike(username)).first():
-            errors.append('This username is already in use.')
+            errors.append("This username is already in use.")
     if not username:
-        errors.append('Username is required.')
+        errors.append("Username is required.")
     else:
         if not re.match(r"^[A-Za-z0-9_]+$", username):
-            errors.append('Usernames are letters, numbers, underscores only.')
+            errors.append("Usernames are letters, numbers, underscores only.")
         if len(username) < 3 or len(username) > 24:
-            errors.append('Username must be between 3 and 24 characters.')
+            errors.append("Username must be between 3 and 24 characters.")
         if User.query.filter(User.username.ilike(username)).first():
-            errors.append('This username is already in use.')
+            errors.append("This username is already in use.")
     if not password:
-        errors.append('Password is required.')
+        errors.append("Password is required.")
     else:
         if len(password) < 5 or len(password) > 256:
-            errors.append('Password must be between 5 and 256 characters.')
+            errors.append("Password must be between 5 and 256 characters.")
     if len(errors) != 0:
-        return render_template("index.html", username=username, email=email, errors=errors, registration=registration)
+        return render_template(
+            "index.html",
+            username=username,
+            email=email,
+            errors=errors,
+            registration=registration,
+        )
     # All good, create an account for them
     user = User(username, email, password)
     user.comments = comments
@@ -81,76 +100,106 @@ def register():
     send_request_notification(user)
     return render_template("index.html", registered=True, registration=registration)
 
-@html.route("/login", methods=['GET', 'POST'])
+
+@html.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'GET':
+    if request.method == "GET":
         if current_user:
-            return redirect("%s://%s/" % (_cfg('protocol'), _cfg('domain')))
-        reset = request.args.get('reset') == '1'
-        return render_template("login.html", **{ 'return_to': request.args.get('return_to'), 'reset': reset })
+            return redirect("%s://%s/" % (_cfg("protocol"), _cfg("domain")))
+        reset = request.args.get("reset") == "1"
+        return render_template(
+            "login.html", **{"return_to": request.args.get("return_to"), "reset": reset}
+        )
     else:
-        username = request.form['username']
-        password = request.form['password']
-        remember = request.form.get('remember-me')
+        username = request.form["username"]
+        password = request.form["password"]
+        remember = request.form.get("remember-me")
         if remember == "on":
             remember = True
         else:
             remember = False
         user = User.query.filter(User.username.ilike(username)).first()
         if not user:
-            return render_template("login.html", **{ "username": username, "errors": 'Your username or password is incorrect.' })
-        if not bcrypt.hashpw(password.encode('UTF-8'), user.password.encode('UTF-8')) == user.password.encode('UTF-8'):
-            return render_template("login.html", **{ "username": username, "errors": 'Your username or password is incorrect.' })
+            return render_template(
+                "login.html",
+                **{
+                    "username": username,
+                    "errors": "Your username or password is incorrect.",
+                }
+            )
+        if not bcrypt.hashpw(
+            password.encode("UTF-8"), user.password.encode("UTF-8")
+        ) == user.password.encode("UTF-8"):
+            return render_template(
+                "login.html",
+                **{
+                    "username": username,
+                    "errors": "Your username or password is incorrect.",
+                }
+            )
         if not user.approved:
-            return redirect("%s://%s/pending" % (_cfg('protocol'), _cfg('domain')))
+            return redirect("%s://%s/pending" % (_cfg("protocol"), _cfg("domain")))
         login_user(user, remember=remember)
-        if 'return_to' in request.form and request.form['return_to']:
-            return redirect(urllib.parse.unquote(request.form.get('return_to')))
-        return redirect("%s://%s/" % (_cfg('protocol'), _cfg('domain')))
+        if "return_to" in request.form and request.form["return_to"]:
+            return redirect(urllib.parse.unquote(request.form.get("return_to")))
+        return redirect("%s://%s/" % (_cfg("protocol"), _cfg("domain")))
+
 
 @html.route("/logout")
 @loginrequired
 def logout():
     logout_user()
-    return redirect("%s://%s/" % (_cfg('protocol'), _cfg('domain')))
+    return redirect("%s://%s/" % (_cfg("protocol"), _cfg("domain")))
+
 
 @html.route("/pending")
 def pending():
     return render_template("pending.html")
+
 
 @html.route("/donate")
 @loginrequired
 def donate():
     return render_template("donate.html")
 
+
 @html.route("/resources")
 @loginrequired
 def resources():
     return render_template("resources.html")
+
 
 @html.route("/script")
 @loginrequired
 def script():
     return render_template("script.html")
 
+
 @html.route("/sharex")
 @loginrequired
 def sharex():
     return render_template("sharex.html")
 
+
 @html.route("/script.plain")
 def script_plain():
     with open("templates/pstepw", "r") as f:
-        resp = f.read().replace('{{ protocol }}', _cfg('protocol'))
-        resp = resp.replace('{{ domain }}', _cfg('domain'))
+        resp = f.read().replace("{{ protocol }}", _cfg("protocol"))
+        resp = resp.replace("{{ domain }}", _cfg("domain"))
     return Response(resp, mimetype="text/plain")
+
 
 @html.route("/approvals")
 @loginrequired
 @adminrequired
 def approvals():
-    users = User.query.filter(User.approved == False).filter(User.rejected == False).order_by(User.created)
+    users = (
+        User.query.filter(User.approved is False)
+        .filter(User.rejected is False)
+        .order_by(User.created)
+    )
     return render_template("approvals.html", users=users)
+
 
 @html.route("/users")
 @loginrequired
@@ -159,13 +208,14 @@ def users():
     users = User.query.filter(User.approved).order_by(User.created).all()
     return render_template("users.html", users=users)
 
-@html.route("/forgot-password", methods=['GET', 'POST'])
+
+@html.route("/forgot-password", methods=["GET", "POST"])
 @with_session
 def forgot_password():
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template("forgot.html")
     else:
-        email = request.form.get('email')
+        email = request.form.get("email")
         if not email:
             return render_template("forgot.html", bad_email=True)
         user = User.query.filter(User.email == email).first()
@@ -177,65 +227,93 @@ def forgot_password():
         send_reset(user)
         return render_template("forgot.html", success=True)
 
-@html.route("/change", methods=['GET', 'POST'])
+
+@html.route("/change", methods=["GET", "POST"])
 @loginrequired
 def change_password():
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template("change.html")
     else:
-        password = request.form.get('password')
-        password2 = request.form.get('password2')
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
         if not password or not password2:
-            return render_template("change.html",errors="Please fill out both fields.")
+            return render_template("change.html", errors="Please fill out both fields.")
         if password != password2:
-            return render_template("change.html",errors="You seem to have mistyped one of these, please try again.")
+            return render_template(
+                "change.html",
+                errors="You seem to have mistyped one of these, please try again.",
+            )
         current_user.set_password(password)
         db.session.commit()
-        return redirect("%s://%s/" % (_cfg('protocol'), _cfg('domain')))
+        return redirect("%s://%s/" % (_cfg("protocol"), _cfg("domain")))
 
-@html.route("/reset", methods=['GET', 'POST'])
-@html.route("/reset/<username>/<confirmation>", methods=['GET', 'POST'])
+
+@html.route("/reset", methods=["GET", "POST"])
+@html.route("/reset/<username>/<confirmation>", methods=["GET", "POST"])
 @with_session
 def reset_password(username, confirmation):
     user = User.query.filter(User.username == username).first()
     if not user:
-        redirect("%s://%s/" % (_cfg('protocol'), _cfg('domain')))
-    if request.method == 'GET':
-        if user.passwordResetExpiry == None or user.passwordResetExpiry < datetime.now():
+        redirect("%s://%s/" % (_cfg("protocol"), _cfg("domain")))
+    if request.method == "GET":
+        if user.passwordResetExpiry is None or user.passwordResetExpiry < datetime.now():
             return render_template("reset.html", expired=True)
         if user.passwordReset != confirmation:
-            redirect("%s://%s/" % (_cfg('protocol'), _cfg('domain')))
+            redirect("%s://%s/" % (_cfg("protocol"), _cfg("domain")))
         return render_template("reset.html", username=username, confirmation=confirmation)
     else:
         if user.passwordResetExpiry == None or user.passwordResetExpiry < datetime.now():
             abort(401)
         if user.passwordReset != confirmation:
             abort(401)
-        password = request.form.get('password')
-        password2 = request.form.get('password2')
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
         if not password or not password2:
-            return render_template("reset.html", username=username, confirmation=confirmation, errors="Please fill out both fields.")
+            return render_template(
+                "reset.html",
+                username=username,
+                confirmation=confirmation,
+                errors="Please fill out both fields.",
+            )
         if password != password2:
-            return render_template("reset.html", username=username, confirmation=confirmation, errors="You seem to have mistyped one of these, please try again.")
+            return render_template(
+                "reset.html",
+                username=username,
+                confirmation=confirmation,
+                errors="You seem to have mistyped one of these, please try again.",
+            )
         user.set_password(password)
         user.passwordReset = None
         user.passwordResetExpiry = None
         db.session.commit()
-        return redirect("%s://%s/login?reset=1" % (_cfg('protocol'), _cfg('domain')))
+        return redirect("%s://%s/login?reset=1" % (_cfg("protocol"), _cfg("domain")))
 
-@html.route("/uploads", methods=['GET'], defaults={'page':1})
-@html.route("/uploads/<page>", methods=['GET'])
+
+@html.route("/uploads", methods=["GET"], defaults={"page": 1})
+@html.route("/uploads/<page>", methods=["GET"])
 @loginrequired
 def uploads(page):
     page = int(page)
-    uploads = db.paginate(db.select(Upload).filter_by(user_id=current_user.id).filter_by(hidden=False).order_by(desc(Upload.created)), page=page, per_page=_cfgi("perpage"))
-    return render_template("uploads.html", pagination=uploads, endpoint='html.uploads')
+    uploads = db.paginate(
+        db.select(Upload)
+        .filter_by(user_id=current_user.id)
+        .filter_by(hidden=False)
+        .order_by(desc(Upload.created)),
+        page=page,
+        per_page=_cfgi("perpage"),
+    )
+    return render_template("uploads.html", pagination=uploads, endpoint="html.uploads")
 
-@html.route("/admin_uploads", methods=['GET'], defaults={'page':1})
-@html.route("/admin_uploads/<page>", methods=['GET'])
+
+@html.route("/admin_uploads", methods=["GET"], defaults={"page": 1})
+@html.route("/admin_uploads/<page>", methods=["GET"])
 @adminrequired
 @loginrequired
 def uploads_admin(page):
     page = int(page)
-    uploads = db.paginate(db.select(Upload).order_by(desc(Upload.created)), page=page, per_page=_cfgi("perpage"))
-    return render_template("admin_uploads.html", pagination=uploads, endpoint='html.uploads_admin')
+    uploads = db.paginate(
+        db.select(Upload).order_by(desc(Upload.created)),
+        page=page,
+        per_page=_cfgi("perpage"),
+    )
+    return render_template("admin_uploads.html", pagination=uploads, endpoint="html.uploads_admin")
