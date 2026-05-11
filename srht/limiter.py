9eb1077fb5230f99823da2afcfdc6dad3f8b1d53
oranges <email@oranges.net.nz>
2026-05-11 20:41:02 +1200

    wip redis limiter

--- srht/limiter.py
+++ srht/limiter.py
@@ -1,8 +1,10 @@
+import os
+
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
     get_remote_address,
     default_limits=[],
-    storage_uri="memory://",
+    storage_uri=os.getenv("REDIS_URL", "memory://"),
 )
